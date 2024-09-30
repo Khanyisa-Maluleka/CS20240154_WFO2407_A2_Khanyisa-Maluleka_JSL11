0b5975c981f476fc930cd2f71b45dca59c0f1de8
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-09-30 19:52:16 +0200

    Imported initial data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TASK: import helper functions from utils
 import { putTask, deleteTask } from "./utils/taskFunctions";
 // TASK: import initialData
-
+import { initialData } from "./initialData";
 
 /*************************************************************************************************************************************************
  * FIX BUGS!!!
