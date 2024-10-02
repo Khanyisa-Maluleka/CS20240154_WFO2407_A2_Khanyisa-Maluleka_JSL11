5a175b63f64e53849f293fa651e2ffc20a3ce1c9
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-10-02 23:27:04 +0200

    Added CSS class to the element tasksContainer

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function filterAndDisplayTasksByBoard(boardName) {
                         </div>`;
 
     const tasksContainer = document.createElement("div");
-  
+    tasksContainer.classList.add("tasks-container");
     column.appendChild(tasksContainer);
 
     filteredTasks.filter(task => task.status === status).forEach(task => { 
